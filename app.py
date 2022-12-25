775f95180f94454c9124607c6e7b224325175c7f
Harsh Mishra <hmisraji07@gmail.com>
2022-12-26 00:52:19 +0530

    Added /download endpoint

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask,request
+from flask import Flask,request,send_file
 import os, gridfs, pika, json
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 from storageAPI import utility
-
+from bson.objectid import ObjectId
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
-mongo = PyMongo(app)
+#Initialise the Mongo Connections for separate databases
+mongo_video_connection=PyMongo(app, uri='mongodb://host.minikube.internal:27017/videos')
+mongo_audio_connection=PyMongo( app, uri='mongodb://host.minikube.internal:27017/mp3s')
 
-fs = gridfs.GridFS(mongo.db)
+fs_videos = gridfs.GridFS(mongo_video_connection.db)
+fs_mp3s = gridfs.GridFS(mongo_audio_connection.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
@@ -28,13 +30,16 @@ def login():
 def upload():
     access, err = validate.token(request)
     access = json.loads(access)
+    
+    if err:
+        return err
 
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
             return "Exactly 1 file per request", 400
 
         for _, f in request.files.items():
-            err = utility.upload(f, fs, channel, access)
+            err = utility.upload(f, fs_videos, channel, access)
 
             if err:
                 return err
@@ -44,8 +49,26 @@ def upload():
     else:
         return "Unauthorised", 403
 
+@app.route("/download", methods=["POST"])
+def download():
+    access,err = validate.token(request)
+    access = json.loads(access)
 
+    if err:
+        return err
 
+    if access["admin"]:
+        fid_string = request.args.get("file_id")
+        if not fid_string:
+            return "file_id required", 400
+        try:
+            output = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(output, download_name=f'{fid_string}.mp3')
+        except Exception as err:
+            print(err)
+            return "Internal Server Error", 500
+
+    return "Unauthorised", 401
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
